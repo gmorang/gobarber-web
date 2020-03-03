d9473dedc32c2d757e8d418780141de7c3f31ec6
Gabriel Morandim <augustomorandim@gmail.com>
2020-03-03 11:07:50 -0300

    authorization token

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -12,14 +12,16 @@ export function* signIn({ payload }) {
 
     const response = yield call(api.post, 'auth', { email, password });
 
-    const { token, user } = response.data;
+    const { jwt, user } = response.data;
 
     if (!user.provider) {
       toast.error('Usuario não é prestador');
       return;
     }
 
-    yield put(signInSuccess(token, user));
+    api.defaults.headers['Authorization'] = `Bearer ${jwt}`;
+
+    yield put(signInSuccess(jwt, user));
 
     history.push('/dashboard');
   } catch (err) {
@@ -42,7 +44,18 @@ export function* signUp({ payload }) {
   }
 }
 
+export function setToken({ payload }) {
+  if (!payload) return;
+
+  const { token } = payload.auth;
+
+  if (token) {
+    api.defaults.headers['Authorization'] = `Bearer ${token}`;
+  }
+}
+
 export default all([
+  takeLatest('persist/REHYDRATE', setToken),
   takeLatest('@auth/SIGN_IN_REQUEST', signIn),
   takeLatest('@auth/SIGN_UP_REQUEST', signUp)
 ]);
