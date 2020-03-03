710d6f4881b34f06665712bf46198c4fb4bf49d0
Gabriel Morandim <augustomorandim@gmail.com>
2020-03-03 10:42:59 -0300

    signup request

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -26,7 +26,23 @@ export function* signIn({ payload }) {
     toast.error('Usuário ou senha incorretos');
     yield put(signFailure());
   }
-
 }
 
-export default all([takeLatest('@auth/SIGN_IN_REQUEST', signIn)]);
+export function* signUp({ payload }) {
+  try {
+    const { name, email, password } = payload
+
+    yield call(api.post, 'users', { name, email, password, provider: true })
+
+    history.push('/');
+  } catch (err) {
+    toast.error('Falha no cadastro, verifique seus dados');
+
+    yield put(signFailure());
+  }
+}
+
+export default all([
+  takeLatest('@auth/SIGN_IN_REQUEST', signIn),
+  takeLatest('@auth/SIGN_UP_REQUEST', signUp)
+]);
